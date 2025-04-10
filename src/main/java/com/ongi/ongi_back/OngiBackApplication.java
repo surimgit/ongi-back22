23cb6e38404a241b665b46475101fa2298744d6c
YooKyuBong <58059368+ykb-i@users.noreply.github.com>
2025-04-10 16:38:40 +0900

    test

---
+++ src/main/java/com/ongi/ongi_back/OngiBackApplication.java
@@ -0,0 +1,13 @@
+package com.ongi.ongi_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngiBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngiBackApplication.class, args);
+	}
+
+}
